eb1de899df0414f8b899445e8cf68805a796ac83
Alongkon Natphunwat <664259031@webmail.npru.ac.th>
2025-09-01 14:29:36 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -1,113 +1,46 @@
-import db from "../models/index.js";
-const User = db.User;
-const Role = db.Role;
-import bcrypt from "bcryptjs";
-import config from "../config/auth.config.js";
-import jwt from "jsonwebtoken";
-import { Op } from "sequelize";
-
-const authController = {};
-
-authController.signUp = async (req, res) => {
-  const { username, name, email, password } = req.body;
-  if (!username || !name || !email || !password) {
-    res.status(400).send({ message: "Please provide all required fields" });
-    return;
-  }
-
-  // SELECT * FROM user WHERE username = username
-  await User.findOne({ where: { username } })
-    // .select(-password)
-    .then((user) => {
-      user && res.status(400).send({ message: "Username is already existed" });
-      return;
-    });
-
-  const newUser = {
-    username,
-    name,
-    email,
-    password: bcrypt.hashSync(password, 8),
-  };
-  User.create(newUser)
-    .then((user) => {
-      // send roles in body [ADMIN]
-      if (req.body.roles) {
-        // SELECT * FROM Role WHERE name = roles1 OR name = roles2
-        Role.findAll({
-          where: {
-            name: { [Op.or]: req.body.roles },
-          },
-        }).then((roles) => {
-          if (roles?.length === 0) {
-            user.setRoles([1]).then(() => {
-              res.send({ message: "User registered successfully3" });
-            });
-          } else {
-            user.setRoles(roles).then(() => {
-              res.send({ message: "User registered successfully1" });
-            });
-          }
-        });
-      } else {
-        user.setRoles([1]).then(() => {
-          res.send({ message: "User registered successfully2" });
-        });
-      }
-    })
-    .catch((error) => {
-      res.status(500).send({
-        message:
-          error.message || "Something error while registering a new user",
-      });
-    });
-};
-
-authController.signIn = async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    res.status(400).send({ message: "Username or password are missing!" });
-    return;
-  }
-  await User.findOne({
-    where: { username: username },
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import restaurantRouter from "./routers/restaurant.router.js";
+import authRouter from "./routers/auth.router.js";
+const FRONTEND_URL = process.env.FRONTEND_URL;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+import cors from "cors";
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "127.0.0.1:5173", FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
   })
-    .then((user) => {
-      //console.log(user);
-      if (!user) {
-        res.status(404).send({ message: "User not found :(" });
-        return;
-      }
-      const passwordIsValid = bcrypt.compareSync(password, user.password);
-      if (!passwordIsValid) {
-        res.status(401).send({ message: "Invalid Password!" });
-      }
-      //Valid User
-      const token = jwt.sign({ username: user.username }, config.secret, {
-        expiresIn: 86400, //60 secs * 60 mins * 24 HOURS = 86400
-      });
-      const authorities = [];
-      user.getRoles().then((roles) => {
-        for (let i = 0; i < roles.length; i++) {
-          //ROLES_USER
-          authorities.push("ROLES_" + roles[i].name.toUpperCase());
-        }
-        res.send({
-          token: token,
-          authorities: authorities,
-          userInfo: {
-            name: user.name,
-            email: user.email,
-            username: user.username,
-          },
-        });
-      });
-    })
-    .catch((error) => {
-      res.status(500).send({
-        message: error.message || "Something error while Signin!",
-      });
-    });
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+import db from "./models/index.js";
+
+const role = db.Role;
+
+const innitRole = () => {
+  role.create({ id: 1, name: "user" });
+  role.create({ id: 2, name: "moderator" });
+  role.create({ id: 3, name: "admin" });
 };
 
-export default authController;
+// db.sequelize.sync({ force: false }).then(() => {
+//   innitRole();
+//   console.log("Drop and Sync");
+// });
+
+app.get("/", (req, res) => {
+  res.send("Restaurant Restful API");
+});
+
+//use router
+app.use("/api/v1/restaurants", restaurantRouter);
+app.use("/api/v1/auth", authRouter);
+
+app.listen(PORT, () => {
+  console.log("Listening to http://localhost:" + PORT);
+});
--- server/models/db.js
+++ server/models/db.js
@@ -6,8 +6,13 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   port: dbConfig.PORT,
   dialect: dbConfig.dialect,
   logging: false,
+  dialectOptions: {
+    ssl: {
+      required: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
-
 const testConnection = async () => {
   try {
     await sequelize.authenticate();
